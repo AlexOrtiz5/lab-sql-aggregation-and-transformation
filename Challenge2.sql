7da1b33efd747a2cf0870bff9f0c4010ed1321a7
Alex Ortiz <alexortizcotto0@gmail.com>
2023-12-19 20:32:45 -0400

    Solution

---
+++ Challenge2.sql
@@ -0,0 +1,53 @@
+-- 1. Next, you need to analyze the films in the collection to gain some 
+-- more insights. Using the film table, determine:
+-- 1.1 The total number of films that have been released.
+SELECT 
+    COUNT(film_id) AS total_films_released
+FROM
+    film;
+-- 1.2 The number of films for each rating.
+SELECT 
+    rating, 
+    COUNT(film_id) AS number_of_films
+FROM
+    film
+GROUP BY rating;
+-- 1.3 The number of films for each rating, sorting the results in 
+-- descending order of the number of films. This will help you to better 
+-- understand the popularity of different film ratings and adjust 
+-- purchasing decisions accordingly.
+SELECT 
+    rating, 
+    COUNT(film_id) AS number_of_films
+FROM
+    film
+GROUP BY rating
+ORDER BY COUNT(film_id) DESC;
+-- 2. Using the film table, determine:
+-- 2.1 The mean film duration for each rating, and sort the results in 
+-- descending order of the mean duration. Round off the average lengths to 
+-- two decimal places. This will help identify popular movie lengths for 
+-- each category.
+SELECT 
+    rating, 
+    ROUND(AVG(length), 2) AS mean_duration
+FROM
+    film
+GROUP BY rating
+ORDER BY mean_duration DESC;
+-- 2.2 Identify which ratings have a mean duration of over two hours in 
+-- order to help select films for customers who prefer longer movies.
+SELECT 
+    rating, 
+    ROUND(AVG(length), 2) AS mean_duration
+FROM
+    film
+GROUP BY rating
+HAVING AVG(length) > 120;
+-- 3. Bonus: determine which last names are not repeated in the table actor.
+SELECT 
+    last_name
+FROM
+    actor
+GROUP BY last_name
+HAVING COUNT(last_name) = 1;
